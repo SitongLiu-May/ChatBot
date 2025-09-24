90b399f6057de3373270e1c934f308689c278674
Sitong Liu <mayliu@bu.edu>
2025-09-24 18:11:39 -0400

    decesion tree logic initiate in js

---
+++ app.js
@@ -0,0 +1,6 @@
+//decision-tree chatbot for a "lost package" scenario
+const chat = document.getElementById("chat");
+const input = document.getElementById("input");
+const sendBtn = document.getElementById("send");
+const chips = document.getElementById("chips");
+
